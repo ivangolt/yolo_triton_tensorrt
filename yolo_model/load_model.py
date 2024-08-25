69b75532dad693ba5aecda33859d01e982f9225d
ivan golt <102701882+ivangolt@users.noreply.github.com>
2024-08-25 17:00:56 +0300

    feat: add load_yolo_model function

---
+++ yolo_model/load_model.py
@@ -0,0 +1,32 @@
+import argparse
+
+from ultralytics import YOLO
+
+
+def load_yolo_model(model_name: str):
+    """Load Yolo model
+
+    Args:
+        model_name (str): yolo model name (e.g. "yolov8n.pt")
+        n - nano model
+        s - small model
+        m - medium model
+        l - large model
+    """
+    model = YOLO(model=model_name)
+    model.export(format="onnx")
+    onnx_model = YOLO(f"{model_name[:7]}.onnx")
+
+    return onnx_model
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="load and convert to onnx yolo model")
+    parser.add_argument(
+        "--model_name", type=str, help="Yolo model name (e.g ''yolov8.pt)"
+    )
+
+    args = parser.parse_args()
+
+    onnx_model = load_yolo_model(model_name=args.model_name)
+    print(f"Model {args.model_name} has been loaded and converted to onnx")
