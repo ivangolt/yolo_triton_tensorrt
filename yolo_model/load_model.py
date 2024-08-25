33c37638b47e5f830d06125236185cce3e16a415
ivan golt <102701882+ivangolt@users.noreply.github.com>
2024-08-25 18:32:07 +0300

    feat: add yolo TensorRT model

--- yolo_model/load_model.py
+++ yolo_model/load_model.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 
 def load_yolo_model(model_name: str):
-    """Load Yolo model
+    """Load Yolo model and transform into onnx and tensorrt engine
 
     Args:
         model_name (str): yolo model name (e.g. "yolov8n.pt")
@@ -13,11 +13,18 @@ def load_yolo_model(model_name: str):
         m - medium model
         l - large model
     """
+    # Return ONNX model
     model = YOLO(model=model_name)
     model.export(format="onnx")
     onnx_model = YOLO(f"{model_name[:7]}.onnx")
 
-    return onnx_model
+    # Return TensorRT model
+    model_trt = YOLO(model=model_name)
+    model_trt.export(format="engine", device=0)
+    tensorrt_model = YOLO(f"{model_name[:7]}.engine")
+
+    # return onnx_model, tensorrt_model
+    return onnx_model, tensorrt_model
 
 
 if __name__ == "__main__":
@@ -28,5 +35,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    onnx_model = load_yolo_model(model_name=args.model_name)
-    print(f"Model {args.model_name} has been loaded and converted to onnx")
+    # onnx_model, tensorrt_model = load_yolo_model(model_name=args.model_name)
+    onnx_model, tensorrt_model = load_yolo_model(model_name=args.model_name)
+
+    print(f"Model {args.model_name} has been loaded and converted to onnx and tensorrt")
