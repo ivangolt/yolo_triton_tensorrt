1bde43db7a7aadbd627c1e21f861ceeb1f176dad
ivan golt <102701882+ivangolt@users.noreply.github.com>
2024-09-01 17:59:38 +0300

    docs: add docs to functions in client.py

--- clients/client.py
+++ clients/client.py
@@ -9,6 +9,14 @@ from utils.yolo_classes import YOLO_CLASSES
 
 
 def get_triton_client(url: str = "localhost:8001"):
+    """Function to implement triton grpc client
+
+    Args:
+        url (_type_, optional): end to end endpoint for inference server. Defaults to "localhost:8001".
+
+    Returns:
+        _type_: triton client
+    """
     try:
         keepalive_options = grpcclient.KeepAliveOptions(
             keepalive_time_ms=2**31 - 1,
@@ -26,6 +34,21 @@ def get_triton_client(url: str = "localhost:8001"):
 
 
 def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+    """
+    Draws a bounding box on an image with a label indicating the class and confidence.
+
+    Parameters:
+    img (numpy.ndarray): The image on which to draw the bounding box.
+    class_id (int): The ID of the detected object's class.
+    confidence (float): The confidence score of the detection.
+    x (int): The x-coordinate of the top left corner of the bounding box.
+    y (int): The y-coordinate of the top left corner of the bounding box.
+    x_plus_w (int): The x-coordinate of the bottom right corner of the bounding box.
+    y_plus_h (int): The y-coordinate of the bottom right corner of the bounding box.
+
+    Returns:
+    None: The function modifies the input image in place.
+    """
     class_name = YOLO_CLASSES[class_id] if class_id < len(YOLO_CLASSES) else "Unknown"
     label = f"({class_name}: {confidence:.2f})"
     color = (
@@ -37,6 +60,25 @@ def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
 
 
 def read_image(image_path: str, expected_image_shape) -> np.ndarray:
+    """
+    Reads an image from the given path, resizes it to the expected shape while maintaining aspect ratio, and prepares it for model input.
+
+    Args:
+        image_path (str): The file path to the image that needs to be read and processed.
+        expected_image_shape (Tuple[int, int]): A tuple representing the expected dimensions (width, height) of the image as required by the model.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, float]: A tuple containing the following elements:
+            - original_image (np.ndarray): The original image read from the file, in its original dimensions.
+            - input_image (np.ndarray): The processed image, resized to the expected dimensions, normalized, and prepared for model input.
+            - scale (float): The scaling factor applied to the original image to match the expected dimensions.
+
+    Example:
+        >>> original_image, input_image, scale = read_image("example.jpg", (640, 640))
+        >>> print(f"Original Image Shape: {original_image.shape}")
+        >>> print(f"Input Image Shape: {input_image.shape}")
+        >>> print(f"Scale Factor: {scale}")
+    """
     expected_width = expected_image_shape[0]
     expected_height = expected_image_shape[1]
     expected_length = min((expected_height, expected_width))
@@ -63,6 +105,31 @@ def run_inference(
     input_image: np.ndarray,
     triton_client: grpcclient.InferenceServerClient,
 ):
+    """
+    Perform inference on an input image using a specified machine learning model deployed on a Triton Inference Server.
+
+    Args:
+        model_name (str): The name of the model deployed on the Triton Inference Server to be used for inference.
+        input_image (np.ndarray): A NumPy array representing the input image to be processed by the model.
+                                  The array should be pre-processed and formatted according to the model's requirements.
+        triton_client (grpcclient.InferenceServerClient): An instance of the Triton Inference Server gRPC client used to
+                                                          communicate with the server and perform the inference.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: A tuple containing the following elements:
+            - num_detections (np.ndarray): A NumPy array indicating the number of detections made by the model.
+            - detection_boxes (np.ndarray): A NumPy array containing the coordinates of the bounding boxes for each detection.
+            - detection_scores (np.ndarray): A NumPy array containing the confidence scores for each detection.
+            - detection_classes (np.ndarray): A NumPy array containing the class IDs for each detected object.
+
+    Example:
+        >>> input_image = preprocess_image("example.jpg")
+        >>> triton_client = grpcclient.InferenceServerClient("localhost:8001")
+        >>> num_detections, boxes, scores, classes = run_inference("yolov5", input_image, triton_client)
+        >>> print(f"Detected {num_detections} objects.")
+        >>> for i in range(num_detections):
+        >>>     print(f"Object {i+1}: Class {classes[i]} with score {scores[i]} at {boxes[i]}")
+    """
     inputs = []
     outputs = []
     inputs.append(grpcclient.InferInput("images", input_image.shape, "FP32"))
